fcbf4d6b2a82cc2d4af0b08378da8a50e93dfd86
Firas Midani <firas.midani@duke.edu>
2018-05-07 14:38:51 -0400

    addedd updateInvWishartDOF()

--- bgmm_library.py
+++ bgmm_library.py
@@ -15,3 +15,19 @@
 import numpy as np
 
 from scipy.stats import dirichlet,invwishart,multinomial,multivariate_normal
+
+def updateInvWishartDOF(data,m):
+	'''
+	updateInvWishartDOF updates the degrees of freedom hyperparameter for Inverse Wishart distribution
+
+	Keyword arguments:
+	data -- n-dimensional data as numpy.ndarray
+	m -- prior degrees of freedom as int or float
+
+	Returns float
+	'''
+
+	n = len(data)
+
+	return n+float(m)
+
