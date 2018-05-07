100794c7e87add92f3f377c022862c4cdf7880ca
Firas Midani <firas.midani@duke.edu>
2018-05-07 15:00:11 -0400

    added updateMVNormalMu()

--- bgmm_library.py
+++ bgmm_library.py
@@ -27,6 +27,7 @@ def updateInvWishartDOF(data,m):
 	Returns float
 	'''
 
+	# number of samples
 	n = len(data)
 
 	return n+float(m)
@@ -36,11 +37,11 @@ def updateInvWishartScaleMatrix(PriorScaleMatrix,SumSquares,DeviationMeans):
     updateInvWishartScaleMatrix updates the scale matrix hyperparameter for the Inverse Wishart distribution
 
     Keyword arguments:
-    PriorScaleMatrix -- prior scale matrix hyperparameter as numpy.ndarray
-    SumSquares -- sum of squares for deviation of data from mean as numpy.ndarray
-    DeviationMeans -- deviation between the prior and estimate mean values as numpy.ndarray
+    PriorScaleMatrix -- prior scale matrix hyperparameter as numpy.ndarray (p x p)
+    SumSquares -- sum of squares for deviation of data from mean as numpy.ndarray (p x p)
+    DeviationMeans -- deviation between the prior and estimate mean values as numpy.ndarray (p x p)
 
-    Returns nump.ndarray
+    Returns nump.ndarray (p x p)
     '''
 
     return PriorScaleMatrix + SumSquares + DeviationMeans
@@ -50,9 +51,9 @@ def computeSumSquares(data):
 	computeSumSquares computes sum of squares for deviation of data from mean
 
 	Keyword arguments:
-	data -- n-dimensional data as numpy.ndarray	
+	data -- n-dimensional data as numpy.ndarray (n x p)
 
-	Returns numpy.ndarray
+	Returns numpy.ndarray (p x p)
 	'''
 
 	# estimate mean values
@@ -61,11 +62,53 @@ def computeSumSquares(data):
     # estimate deviation from mean
     deviations = np.matrix(data - y_bar);
     
-    # dot product of deviations from mean
+    # compute squares of deviations from mean
     Squares = [deviation.transpose()*deviation for deviation in deviations]
 
-    # sum deviationd ot products
+    # sum deviations of dot products
     SumSquares = np.sum(Squares,axis=0)
     
     return SumSquares
+
+def computeDeviationMeans(data,PriorMean):
+    '''
+    computeDeviationMeans compute squares for deviation between prior and esitmated mean values
+
+    Keyword arguments:
+    data -- n-dimensional data as numpy.ndarray (n x p)
+    PriorMean -- prior mean values (p x 1)
+
+    Returns numpy.ndarray (p x p)
+    '''
+
+	# estimate mean values
+    y_bar = np.mean(data,axis=0);
+
+    # estimate deviation from prior mean value
+    deviations = np.matrix(y_bar - PriorMean.transpose());
+
+    # compute squares of deviations from prior mean
+    Squares = deviation.transpose()*deviation
+    
+    return Squares
+    
+def updateMVNormalMu(data,precision,PriorMean):
+     '''
+    updateMVNormalMu updates the mean hyperparamter for MultiVariate Normal distribution
+
+    Keyword arguments:
+    data -- n-dimensional data as numpy.ndarray (n x p)
+    precision -- prior precision hyperparameter (1 x 1) 
+    PriorMean -- prior mean values (p x 1)
+
+    Returns numpy.ndarray (p x p)
+    '''
+   
+   	# number of samples
+    n = len(data);
+
+	# estimate mean values
+    y_bar = np.mean(data,axis=0); # (1 x p)
+    
+    return (np.dot(precision,PriorMean).transpose()+np.dot(n,y_bar))/(n+precision)
     
