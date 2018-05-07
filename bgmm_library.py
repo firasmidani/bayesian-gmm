5be072b429060429298553de3e93604fa5248829
Firas Midani <fsm3@duke.edu>
2018-05-07 11:05:13 -0400

    Initialize bgmm_library.py

---
+++ bgmm_library.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# Firas Said Midani
+# Start date: 2018-05-27
+# Final date: 2018-05-07
+
+# DESCRIPTION Library of functions for analysis of flow cytometry data
+
+# TABLE OF CONTENTS
+#
+#
+
+# IMPORT NECESSARY LIBRARIES
+
+import numpy as np
+
+from scipy.stats import dirichlet,invwishart,multinomial,multivariate_normal
